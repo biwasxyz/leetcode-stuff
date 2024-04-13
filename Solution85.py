978c74a663b9bb1138d2e62ae675fa195d3460c2
xbiwas <biwas2059@gmail.com>
2024-04-13 22:00:50 +0545

    Solution of Problem 85. Maximal Rectangle

---
+++ Solution85.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def maximalRectangle(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        if not matrix:
+            return 0
+        
+        rows = len(matrix)
+        cols = len(matrix[0])
+        heights = [0] * cols
+        max_area = 0
+        
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == '1':
+                    heights[j] += 1
+                else:
+                    heights[j] = 0
+                    
+            max_area = max(max_area, self.largestRectangleArea(heights))
+        
+        return max_area
+    
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        heights.append(0)  
+        stack = [-1]  
+        max_area = 0
+        
+        for i in range(len(heights)):
+            while heights[i] < heights[stack[-1]]:
+                h = heights[stack.pop()]
+                w = i - stack[-1] - 1
+                max_area = max(max_area, h * w)
+            stack.append(i)
+        
+        heights.pop() 
+        return max_area
